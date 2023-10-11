ed5d1e1ac16c8df262488c78f95d2071c98d903c
Dylan Elliott <dylan.elliott@gmail.com>
2023-10-11 13:07:12 +1100

    Initial commit

---
+++ Journy/JournyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournyApp.swift
+//  Journy
+//
+//  Created by Dylan Elliott on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct JournyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
